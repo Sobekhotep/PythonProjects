a53a236beb1079aec550838ffadb99bab692a605
Sobekhotep <vlad.zankevich@gmail.com>
2017-09-19 22:05:13 +0300

    modified Sum Number Function

--- SumNumbersWhithDivisorFunction.py
+++ SumNumbersWhithDivisorFunction.py
@@ -3,7 +3,11 @@ def fdivisor(down, up, divisor):
     for i in range(int(down), int(up)+1):
         if i % divisor == 0:
             list.append(i)
-    summa = sum(list)
-    print (summa)
 
-fdivisor(1, 1000, 7)
+    print (sum(list))
+
+fdivisor(1, 1000, 17)
+fdivisor(1, 1000, 21)
+fdivisor(1, 1000, 33)
+fdivisor(1, 1000, 47)
+fdivisor(1, 1000, 55)
