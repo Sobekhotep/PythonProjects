5986b85ec16018ace010032d4203cf0294de969f
Sobekhotep <vlad.zankevich@gmail.com>
2017-09-20 15:22:03 +0300

    changed sum number function commit 2

--- SumNumbersWhithDivisorFunction.py
+++ SumNumbersWhithDivisorFunction.py
@@ -1,12 +1,12 @@
-def fdivisor(down, up, divisor):
+def fdiv(down, up, div):
     a = 0
-    for i in range(int(down), int(up)+1):
-        if i % divisor == 0:
+    for i in range(down, up+1):
+        if i % div == 0:
             a = a + i
     return a
 
-print(fdivisor(1, 1000, 17))
-print(fdivisor(1, 1000, 21))
-print(fdivisor(1, 1000, 33))
-print(fdivisor(1, 1000, 47))
-print(fdivisor(1, 1000, 55))
+print(fdiv(1, 1000, 17))
+print(fdiv(1, 1000, 21))
+print(fdiv(1, 1000, 33))
+print(fdiv(1, 1000, 47))
+print(fdiv(1, 1000, 55))
