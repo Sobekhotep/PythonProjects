b6e09d6871fee7cb7ce07177b938bfc59f18df73
Sobekhotep <vlad.zankevich@gmail.com>
2017-09-22 17:23:34 +0300

    Add file with weather of Kiev but yet not complete

--- Weather_forecast_for_Kiev.py
+++ Weather_forecast_for_Kiev.py
@@ -2,19 +2,17 @@ import requests
 import xml.etree.ElementTree as ET
 
 url = requests.get('http://www.eurometeo.ru/ukraina/kiyvska-oblast/kiyiv/export/xml/data/')
+root = ET.fromstring(url.text)
 
-file = open('/home/vlad/Documents/WeatherOfKiev.xml', 'w')
-for index in url.text:
-    file.write(index)
-file.close()
+day = int(input('На какой день Вы хотите узнать прогноз? Если на сегодня - введите цифру 1, '
+          'на завтра - цифру 2, на послезавтра - цифру 3: '))
 
-tree = ET.parse('/home/vlad/Documents/WeatherOfKiev.xml')
-root = tree.getroot()
-
-for child in root:
-    print(child.tag, child.attrib)
+time = int(input('Какое время Вас интересует? Если утро - введите цифру 1, день - цифру 2, '
+             'вечер - цифру 3, ночь - цифру 4: '))
 
 
-
-
-#print (url.content)
+if day == 1 and time == 1:
+    print ('Давление: ', root[0][4][1].text, '\nТемпература:', root[0][4][2].text)
+#elif day == 2 and time == 1:
+#    print ('Давление: ', root[0][4][1].text, '\nТемпература:', root[0][4][2].text)
+#    print ('Извините, прогноза погоды на такой день у нас нет, попробуйте ещё раз.')
