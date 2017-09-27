23ae88f7dcad5f80d05761fe1ca3d40154f3d484
Sobekhotep <vlad.zankevich@gmail.com>
2017-09-27 22:57:59 +0300

    Changed weather_forecast.py

--- weather_forecast.py
+++ weather_forecast.py
@@ -1,6 +1,14 @@
 import requests
 import xml.etree.ElementTree as ET
 
+day_list = [1, 2, 3]
+time_list = [1, 2, 3, 4]
+
+night_value = 1
+morning_value = 2
+afternoon_value = 3
+evening_value = 4
+
 class Forecast():
     """Weather forecast for city or town for which url is entered"""
 
@@ -10,81 +18,76 @@ class Forecast():
     def today(self, night=False, morning=False, afternoon=False, evening=False):
         response = requests.get(self.url)
         root = ET.fromstring(response.text)
-        if night == True:
-            print('\nПрогноз погоды в г.', root[0][1].text,
-                                   '\nна', root[0][4][0].text,
-                ' \nТемпература воздуха:', root[0][4][2].text + ' градусов',
-               '\nАтмосферное давление: ', root[0][4][1].text + ' мм ртутного столба')
-        elif morning == True:
-            print('\nПрогноз погоды в г.', root[0][1].text,
-                                   '\nна', root[0][5][0].text,
-                ' \nТемпература воздуха:', root[0][5][2].text + ' градусов',
-               '\nАтмосферное давление: ', root[0][5][1].text + ' мм ртутного столба')
-        elif afternoon == True:
-            print('\nПрогноз погоды в г.', root[0][1].text,
-                                   '\nна', root[0][6][0].text,
-                ' \nТемпература воздуха:', root[0][6][2].text + ' градусов',
-               '\nАтмосферное давление: ', root[0][6][1].text + ' мм ртутного столба')
-        elif evening == True:
-            print('\nПрогноз погоды в г.', root[0][1].text,
-                                   '\nна', root[0][7][0].text,
-                ' \nТемпература воздуха:', root[0][7][2].text + ' градусов',
-               '\nАтмосферное давление: ', root[0][7][1].text + ' мм ртутного столба')
-        else:
-            print('Что-то пошло не так, звоните в милицию.')
+        root_city = root[0][1].text
+        root_time = root[0][4 + int(night)*night_value + int(morning)*morning_value +
+                            int(afternoon)*afternoon_value + int(evening)*evening_value - 1][0].text
+        root_temp = root[0][4 + int(night)*night_value + int(morning)*morning_value +
+                            int(afternoon)*afternoon_value + int(evening)*evening_value - 1][2].text
+        root_press = root[0][4 + int(night)*night_value + int(morning)*morning_value +
+                             int(afternoon)*afternoon_value + int(evening)*evening_value - 1][1].text
+
+        print ('\nПрогноз погоды в г.', root_city,
+                                '\nна', root_time,
+             ' \nТемпература воздуха:', root_temp + ' градусов',
+            '\nАтмосферное давление: ', root_press + ' мм ртутного столба')
+        return ('Спасибо!')
 
     def tomorrow(self, night=False, morning=False, afternoon=False, evening=False):
         response = requests.get(self.url)
         root = ET.fromstring(response.text)
-        if night == True:
-            print('\nПрогноз погоды в г.', root[0][1].text,
-                                   '\nна', root[0][8][0].text,
-                ' \nТемпература воздуха:', root[0][8][2].text + ' градусов',
-               '\nАтмосферное давление: ', root[0][8][1].text + ' мм ртутного столба')
-        elif morning == True:
-            print('\nПрогноз погоды в г.', root[0][1].text,
-                                   '\nна', root[0][9][0].text,
-                ' \nТемпература воздуха:', root[0][9][2].text + ' градусов',
-               '\nАтмосферное давление: ', root[0][9][1].text + ' мм ртутного столба')
-        elif afternoon == True:
-            print('\nПрогноз погоды в г.', root[0][1].text,
-                                   '\nна', root[0][10][0].text,
-                ' \nТемпература воздуха:', root[0][10][2].text + ' градусов',
-               '\nАтмосферное давление: ', root[0][10][1].text + ' мм ртутного столба')
-        elif evening == True:
-            print('\nПрогноз погоды в г.', root[0][1].text,
-                                   '\nна', root[0][11][0].text,
-                ' \nТемпература воздуха:', root[0][11][2].text + ' градусов',
-               '\nАтмосферное давление: ', root[0][11][1].text + ' мм ртутного столба')
-        else:
-            print('Что-то пошло не так, звоните в милицию.')
+        root_city = root[0][1].text
+        root_time = root[0][8 + int(night)*night_value + int(morning)*morning_value +
+                            int(afternoon)*afternoon_value + int(evening)*evening_value - 1][0].text
+        root_temp = root[0][8 + int(night)*night_value + int(morning)*morning_value +
+                            int(afternoon)*afternoon_value + int(evening)*evening_value - 1][2].text
+        root_press = root[0][8 + int(night)*night_value + int(morning)*morning_value +
+                             int(afternoon)*afternoon_value + int(evening)*evening_value - 1][1].text
+
+        print ('\nПрогноз погоды в г.', root_city,
+                                '\nна', root_time,
+             ' \nТемпература воздуха:', root_temp + ' градусов',
+            '\nАтмосферное давление: ', root_press + ' мм ртутного столба')
+        return ('Спасибо!')
 
     def after_tomorrow(self, night=False, morning=False, afternoon=False, evening=False):
         response = requests.get(self.url)
         root = ET.fromstring(response.text)
-        if night == True:
-            print('\nПрогноз погоды в г.', root[0][1].text,
-                                   '\nна', root[0][12][0].text,
-                ' \nТемпература воздуха:', root[0][12][2].text + ' градусов',
-               '\nАтмосферное давление: ', root[0][12][1].text + ' мм ртутного столба')
-        elif morning == True:
-            print('\nПрогноз погоды в г.', root[0][1].text,
-                                   '\nна', root[0][13][0].text,
-                ' \nТемпература воздуха:', root[0][13][2].text + ' градусов',
-               '\nАтмосферное давление: ', root[0][13][1].text + ' мм ртутного столба')
-        elif afternoon == True:
-            print('\nПрогноз погоды в г.', root[0][1].text,
-                                   '\nна', root[0][14][0].text,
-                ' \nТемпература воздуха:', root[0][14][2].text + ' градусов',
-               '\nАтмосферное давление: ', root[0][14][1].text + ' мм ртутного столба')
-        elif evening == True:
-            print('\nПрогноз погоды в г.', root[0][1].text,
-                                   '\nна', root[0][15][0].text,
-                ' \nТемпература воздуха:', root[0][15][2].text + ' градусов',
-               '\nАтмосферное давление: ', root[0][15][1].text + ' мм ртутного столба')
+        root_city = root[0][1].text
+        root_time = root[0][12 + int(night)*night_value + int(morning)*morning_value +
+                            int(afternoon)*afternoon_value + int(evening)*evening_value - 1][0].text
+        root_temp = root[0][12 + int(night)*night_value + int(morning)*morning_value +
+                            int(afternoon)*afternoon_value + int(evening)*evening_value - 1][2].text
+        root_press = root[0][12 + int(night)*night_value + int(morning)*morning_value +
+                             int(afternoon)*afternoon_value + int(evening)*evening_value - 1][1].text
+
+        print ('\nПрогноз погоды в г.', root_city,
+                                '\nна', root_time,
+             ' \nТемпература воздуха:', root_temp + ' градусов',
+            '\nАтмосферное давление: ', root_press + ' мм ртутного столба')
+        return ('Спасибо!')
+
+    def forecast(self, day, night=False, morning=False, afternoon=False, evening=False):
+        response = requests.get(self.url)
+        root = ET.fromstring(response.text)
+        if day in day_list:
+            pass
         else:
             print('Что-то пошло не так, звоните в милицию.')
+            raise SystemExit(1)
 
+        root_city = root[0][1].text
+        root_time = root[0][day*4 + int(night)*night_value + int(morning)*morning_value +
+                            int(afternoon)*afternoon_value + int(evening)*evening_value - 1][0].text
+        root_temp = root[0][day*4 + int(night)*night_value + int(morning)*morning_value +
+                            int(afternoon)*afternoon_value + int(evening)*evening_value - 1][2].text
+        root_press = root[0][day*4 + int(night)*night_value + int(morning)*morning_value +
+                             int(afternoon)*afternoon_value + int(evening)*evening_value - 1][1].text
+
+        print ('\nПрогноз погоды в г.', root_city,
+                                '\nна', root_time,
+             ' \nТемпература воздуха:', root_temp + ' градусов',
+            '\nАтмосферное давление: ', root_press + ' мм ртутного столба')
+        return ('Спасибо!')
 
 forecast = Forecast('http://www.eurometeo.ru/belarus/gomelskaya-oblast/jitkovichi/export/xml/data/')
-print(forecast.after_tomorrow(evening=True))
+print(forecast.after_tomorrow(evening = True))
