f0ee9cb5bffc7b89aaada1c650c3d57a2f6e5677
Sobekhotep <vlad.zankevich@gmail.com>
2017-09-29 13:38:07 +0300

    Changed weather_forecast.py with **kwargs

--- weather_forecast.py
+++ weather_forecast.py
@@ -1,13 +1,6 @@
 import requests
 import xml.etree.ElementTree as ET
 
-DAY_LIST = (1, 2, 3)
-
-NIGHT_VALUE = 1
-MORNING_VALUE = 2
-AFTERNOON_VALUE = 3
-EVENING_VALUE = 4
-
 class Forecast():
     """Weather forecast for city or town for which url is entered"""
 
@@ -15,21 +8,28 @@ class Forecast():
         self.url = url
 
     def forecast(self, day, night=False, morning=False, afternoon=False, evening=False):
+
+        day_list = (1, 2, 3)
+        night_value = 1
+        morning_value = 2
+        afternoon_value = 3
+        evening_value = 4
+
         response = requests.get(self.url)
         root = ET.fromstring(response.text)
-        if day in DAY_LIST:
+        if day in day_list:
             pass
         else:
             print('Что-то пошло не так, звоните в милицию.')
             raise SystemExit(1)
 
         root_city = root[0][1].text
-        root_time = root[0][day*4 + int(night)*NIGHT_VALUE + int(morning)*MORNING_VALUE +
-                            int(afternoon)*AFTERNOON_VALUE + int(evening)*EVENING_VALUE - 1][0].text
-        root_temp = root[0][day*4 + int(night)*NIGHT_VALUE + int(morning)*MORNING_VALUE +
-                            int(afternoon)*AFTERNOON_VALUE + int(evening)*EVENING_VALUE - 1][2].text
-        root_press = root[0][day*4 + int(night)*NIGHT_VALUE + int(morning)*MORNING_VALUE +
-                            int(afternoon)*AFTERNOON_VALUE + int(evening)*EVENING_VALUE - 1][1].text
+        root_time = root[0][day*4 + int(night)*night_value + int(morning)*morning_value +
+                            int(afternoon)*afternoon_value + int(evening)*evening_value - 1][0].text
+        root_temp = root[0][day*4 + int(night)*night_value + int(morning)*morning_value +
+                            int(afternoon)*afternoon_value + int(evening)*evening_value - 1][2].text
+        root_press = root[0][day*4 + int(night)*night_value + int(morning)*morning_value +
+                            int(afternoon)*afternoon_value + int(evening)*evening_value - 1][1].text
 
         text1 = "Прогноз погоды по г. "
         text2 = " на "
@@ -43,14 +43,14 @@ class Forecast():
 
         return message
 
-    def today(self, night=False, morning=False, afternoon=False, evening=False):
-        return self.forecast(day=1, night=night, morning=morning, afternoon=afternoon, evening=evening)
+    def today(self, **kwargs):
+        return self.forecast(day=1, **kwargs)
 
-    def tomorrow(self, night=False, morning=False, afternoon=False, evening=False):
-        return self.forecast(day=2, night=night, morning=morning, afternoon=afternoon, evening=evening)
+    def tomorrow(self, **kwargs):
+        return self.forecast(day=2, **kwargs)
 
-    def after_tomorrow(self, night=False, morning=False, afternoon=False, evening=False):
-        return self.forecast(day=3, night=night, morning=morning, afternoon=afternoon, evening=evening)
+    def after_tomorrow(self, **kwargs):
+        return self.forecast(day=3, **kwargs)
 
 forecast = Forecast('http://www.eurometeo.ru/belarus/gomelskaya-oblast/jitkovichi/export/xml/data/')
-print(forecast.today(night=True))
+print(forecast.after_tomorrow(afternoon=True))
