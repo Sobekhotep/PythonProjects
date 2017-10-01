4d54cda38dc9f3140e6d83152feaac29d3809e51
Sobekhotep <vlad.zankevich@gmail.com>
2017-10-01 21:02:11 +0300

    Try to add class ForecastResult

--- weather_forecast.py
+++ weather_forecast.py
@@ -1,6 +1,16 @@
 import requests
 import xml.etree.ElementTree as ET
 
+
+class ForecastResult():
+    def __init__(self, url):
+        self.url = url
+
+    def __str__(self):
+        response = requests.get(self.url)
+        root = ET.fromstring(response.text)
+        return root
+
 class ForecastFetcher():
     """Weather forecast for city or town for which url is entered"""
 
@@ -9,7 +19,7 @@ class ForecastFetcher():
 
     def forecast(self, day, night=False, morning=False, afternoon=False, evening=False):
 
-        day_list = (1, 2, 3)
+        day_tuple = (1, 2, 3)
         night_value = 1
         morning_value = 2
         afternoon_value = 3
@@ -17,7 +27,7 @@ class ForecastFetcher():
 
         response = requests.get(self.url)
         root = ET.fromstring(response.text)
-        if day in day_list:
+        if day in day_tuple:
             pass
         else:
             print('Что-то пошло не так, звоните в милицию.')
@@ -46,5 +56,9 @@ class ForecastFetcher():
     def after_tomorrow(self, **kwargs):
         return self.forecast(day=3, **kwargs)
 
+
 forecast = ForecastFetcher('http://www.eurometeo.ru/belarus/gomelskaya-oblast/jitkovichi/export/xml/data/')
 print(forecast.tomorrow(night=True))
+
+forecast = ForecastResult('http://www.eurometeo.ru/belarus/gomelskaya-oblast/jitkovichi/export/xml/data/')
+print(forecast.__str__())
